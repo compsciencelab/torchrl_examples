8d548c8514bb9c210ca9bb111dc0431488339fde
vmoens <vincentmoens@gmail.com>
2023-01-18 10:41:07 +0000

    init

--- ppo_pong/ppo.py
+++ ppo_pong/ppo.py
@@ -1,6 +1,8 @@
+import gc
 import time
 import yaml
 import wandb
+import tqdm
 import torch
 import argparse
 
@@ -8,13 +10,11 @@ import argparse
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.envs import TransformedEnv
 from torchrl.envs.vec_env import ParallelEnv
-from torchrl.envs.transforms import ToTensorImage, GrayScale, CatFrames, NoopResetEnv, Resize
-from .transforms.reward_sum import RewardSum
-from .transforms.step_limit import StepLimit
+from torchrl.envs.transforms import ToTensorImage, GrayScale, CatFrames, NoopResetEnv, Resize, ObservationNorm, RewardSum, StepCounter
 
 # Model imports
 from torchrl.envs import EnvCreator
-from torchrl.envs.utils import set_exploration_mode
+from torchrl.envs.utils import set_exploration_mode, check_env_specs
 from torchrl.modules.models import ConvNet, MLP
 from torchrl.modules.distributions import OneHotCategorical
 from torchrl.modules import SafeModule, ProbabilisticActor, ValueOperator, ActorValueOperator
@@ -30,6 +30,7 @@ from torchrl.objectives.value.advantages import GAE
 from torch.optim import Adam
 from torch.optim.lr_scheduler import LinearLR
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
+import torch.distributions as dist
 
 
 def main():
@@ -37,14 +38,15 @@ def main():
     args = get_args()
 
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    device_collection = torch.device("cuda:1") if torch.cuda.device_count() > 1 else torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
     # 1. Define environment --------------------------------------------------------------------------------------------
 
     # 1.1 Define env factory
-    def env_factory():
+    def env_factory(device=device):
         """Creates an instance of the environment."""
 
-        create_env_fn = EnvCreator(lambda: GymEnv(env_name=args.env_name, frame_skip=args.frame_skip))
+        create_env_fn = EnvCreator(lambda: GymEnv(env_name=args.env_name, frame_skip=args.frame_skip, categorical_action_encoding=True))
 
         # 1.2 Create env vector
         vec_env = ParallelEnv(create_env_fn=create_env_fn, num_workers=args.num_parallel_envs)
@@ -55,14 +57,23 @@ def main():
         transformed_vec_env.append_transform(ToTensorImage())  # change shape from [h, w, 3] to [3, h, w]
         transformed_vec_env.append_transform(Resize(w=84, h=84))  # Resize image
         transformed_vec_env.append_transform(GrayScale())  # Convert to Grayscale
+        transformed_vec_env.append_transform(ObservationNorm(in_keys=["pixels"], standard_normal=True))
         transformed_vec_env.append_transform(CatFrames(N=4))  # Stack last 4 frames
         transformed_vec_env.append_transform(RewardSum())
-        transformed_vec_env.append_transform(StepLimit())
+        transformed_vec_env.append_transform(StepCounter())
 
-        return transformed_vec_env
+        norm_layer = transformed_vec_env.transform[3]
+        norm_layer.init_stats(num_iter=1000, reduce_dim=[0, 1, 3, 4], cat_dim=0, keep_dims=[3, 4])
+
+        return transformed_vec_env.to(device)
 
     # Sanity check
     test_env = env_factory()
+    train_env = env_factory()
+    check_env_specs(train_env)
+
+    test_env.load_state_dict(train_env.state_dict())
+
     test_input = test_env.reset()
     assert "pixels" in test_input.keys()
     num_actions = test_env.specs["action_spec"].space.n
@@ -80,7 +91,7 @@ def main():
         num_cells=[32, 64, 64],
         kernel_sizes=[8, 4, 3],
         strides=[4, 2, 1],
-    )
+    ).to(device)
     common_cnn_output = common_cnn(torch.ones_like(test_input["pixels"]))
 
     # Add MLP on top of shared MLP
@@ -89,7 +100,7 @@ def main():
         activation_class=torch.nn.ReLU,
         activate_last_layer=True,
         out_features=448,
-        num_cells=[256])
+        num_cells=[256]).to(device)
     common_mlp_output = common_mlp(common_cnn_output)
 
     # Define shared net as TensorDictModule
@@ -106,7 +117,7 @@ def main():
         in_features=common_mlp_output.shape[-1],
         out_features=num_actions,
         num_cells=[]
-    )
+    ).to(device)
 
     # Define TensorDictModule
     policy_module = SafeModule(  # TODO: The naming of SafeModule is confusing
@@ -120,7 +131,7 @@ def main():
         policy_module,
         in_keys=["logits"],  # TODO: Seems like only "logits" can be used as in_keys
         #        out_keys=["action"],
-        distribution_class=OneHotCategorical,
+        distribution_class=dist.Categorical,
         distribution_kwargs={},
         return_log_prob=True,
     )
@@ -132,7 +143,7 @@ def main():
         in_features=common_mlp_output.shape[-1],
         out_features=1,
         num_cells=[]
-    )
+    ).to(device)
 
     # Define TensorDictModule
     value_module = ValueOperator(
@@ -154,14 +165,15 @@ def main():
         td = td.to(device)
         td = actor_critic(td)
 
+    print(actor_critic)
     # TODO: why wrap them together and then get separate operators ?
     # Get independent operators for actor and critic, to be able to call only one of them
     actor = actor_critic.get_policy_operator()
     critic = actor_critic.get_value_operator()
 
     # sanity check
-    actor(env_factory().reset())
-    actor_critic(env_factory().reset())
+    actor(test_env.reset())
+    actor_critic(test_env.reset())
 
     # # Ugly hack, otherwise I get errors
     # critic.out_keys = ['state_value', 'common_features']
@@ -170,11 +182,13 @@ def main():
     # 2. Define Collector ----------------------------------------------------------------------------------------------
 
     collector = SyncDataCollector(
-        create_env_fn=env_factory,
+        create_env_fn=train_env,
         create_env_kwargs=None,
         policy=actor_critic,
         total_frames=args.total_frames,
         frames_per_batch=args.steps_per_env * args.num_parallel_envs,
+        device=device_collection,
+        passing_device=device_collection,
     )
 
     # 3. Define Loss ---------------------------------------------------------------------------------------------------
@@ -203,7 +217,7 @@ def main():
         mode = "online"
         wandb.login(key=str(args.wandb_key))
     else:
-        mode = "disabled"
+        mode = "offline"
 
     # 5. Define training loop ------------------------------------------------------------------------------------------
 
@@ -216,17 +230,18 @@ def main():
     scheduler = LinearLR(optimizer, total_iters=total_network_updates, start_factor=1.0, end_factor=0.1)
     evaluation_frequency = 100  # In number of network frames
 
-    with wandb.init(project=args.experiment_name, name=args.agent_name, config=args, mode=mode):
+    with wandb.init(project=args.experiment_name, name=args.agent_name, entity=args.entity, config=args, mode=mode):
 
+        pbar = tqdm.tqdm(total=collector.total_frames)
         start_time = time.time()
 
         for batch in collector:
-
+            batch = batch.cpu()
             log_info = {}
-
 
             # We don't use memory networks, so sequence dimension is not relevant
             batch_size = batch["mask"].sum().item()
+            pbar.update(batch_size)
             collected_frames += batch_size
 
             # add episode reward info
@@ -239,7 +254,7 @@ def main():
             for epoch in range(args.num_ppo_epochs):
 
                 # Compute advantage with the whole batch
-                batch = advantage_module(batch)
+                batch = advantage_module(batch.to(device))
 
                 batch_view = batch[batch["mask"].squeeze(-1)]
 
@@ -248,7 +263,7 @@ def main():
                         SubsetRandomSampler(range(batch_size)), args.mini_batch_size, drop_last=True):
 
                     # select idxs to create mini_batch
-                    mini_batch = batch_view[mini_batch_idxs].clone()
+                    mini_batch = batch_view[mini_batch_idxs].clone().to(device)
 
                     # Forward pass
                     loss = loss_module(mini_batch)
@@ -271,6 +286,7 @@ def main():
                         "loss_objective": loss["loss_objective"].item(),
                         "learning_rate": float(scheduler.get_last_lr()[0]),
                         "collected_frames": collected_frames,
+                        "reward": batch["reward"][batch["mask"]].mean().item(),
                     })
 
                     if network_updates % args.evaluation_frequency == 0 and network_updates != 0:
@@ -288,10 +304,10 @@ def main():
                     # Print an informative message in the terminal
                     fps = int(collected_frames * args.frame_skip / (time.time() - start_time))
                     print_msg = f"Update {network_updates}, num " \
-                                f"samples collected {collected_frames * args.frame_skip}, FPS {fps}\n    "
+                                f"samples collected {collected_frames * args.frame_skip}, FPS {fps} "
                     for k, v in log_info.items():
-                        print_msg += f"{k}: {v} "
-                    print(print_msg, flush=True)
+                        print_msg += f"{k}: {v: 4.4f} "
+                    pbar.set_description(print_msg)
 
                     log_info.update({"collected_frames": int(collected_frames * args.frame_skip), "fps": fps})
                     wandb.log(log_info, step=network_updates)
@@ -299,6 +315,7 @@ def main():
 
             # Update collector weights!
             collector.update_policy_weights_()
+            gc.collect()
 
 
 def get_args():
